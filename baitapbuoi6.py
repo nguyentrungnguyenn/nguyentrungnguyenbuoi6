3c335443ef751aced4e71554469f229b128e0ef5
nguyentrungnguyenn <ngtrungnguyen210@gmail.com>
2024-08-20 23:45:36 +0700

    Add files via upload

---
+++ baitapbuoi6.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 20 20:06:40 2024
+
+@author: Nguyễn Trung Nguyên
+Bài 1: Chuỗi bài 1.1
+
+Dia_chi = "Đại học Quốc gia, Khu phố 6, P. Linh Trung, Q. Thủ Đức, Tp. HCM"
+split_1 = "Đại học Quốc gia"
+split_2 = "Khu phố 6"
+split_3 = "P. Linh Trung"
+split_4 = "Q. Thủ Đức"
+split_5 = "Tp. HCM"
+len(Dia_chi)
+print("", split_1)
+print("", split_2)
+print("", split_3)
+print("", split_4)
+print("", split_5)
+
+Bài 2: Chuỗi bài 2.2
+
+var = "Đại học Quốc gia, Khu phố 6, P. Linh Trung, Q. Thủ Đức, Tp. HCM"
+print("", var[0:16])
+print("", var[18:27])
+print("", var[32:42])
+print("", var[47:54])
+print("", var[60:63])
+
+Bài 3: Con mèo
+    
+input = "iam a cat"
+cat = input.title()
+print(input.title())
+print(input.upper())
+print(cat.swapcase())
+print(input.lower())
+
+"""
+
